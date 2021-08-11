b4327fdbd45ae3fbe20d1800fee73541da4634db
Alec Campbell <88142969+22940dev@users.noreply.github.com>
2021-08-11 06:53:06 -0400

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -3,7 +3,7 @@ function Header() {
     <header className="container flex text-center mx-auto py-10">
       <div className="md:w-4/6 mx-auto">
         <img src="/profile-pic.jpg" alt="Profile Picture" className="h-24 rounded-full mx-auto mb-5" />
-        <h1 className="font-bold">Pieter Montoulieu</h1>
+        <h1 className="font-bold">22940dev</h1>
         <p>Full Stack Engineer, Game Developer and Audio Engineer</p>
       </div>
     </header>
